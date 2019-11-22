0c6aeff3b36519f893753d2cf96870df1f3d85aa
Eric S. Londres <elondres@stevens.edu>
2019-11-22 12:05:31 -0500

    Updates

---
+++ precache-manifest.788354666f623592f72b3cd02204fc22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034330c85e0ef07a56bd7dc26727c5dd",
+    "url": "/front-end/index.html"
+  },
+  {
+    "revision": "2cbd1d0da8bf0ef6032f",
+    "url": "/front-end/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8974e96ac421c2e39278",
+    "url": "/front-end/static/js/2.1f603132.chunk.js"
+  },
+  {
+    "revision": "2cbd1d0da8bf0ef6032f",
+    "url": "/front-end/static/js/main.0428e85c.chunk.js"
+  },
+  {
+    "revision": "078bf7eecbcf02ac2053",
+    "url": "/front-end/static/js/runtime-main.33fd11a9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/front-end/static/media/logo.25bf045c.svg"
+  }
+]);
